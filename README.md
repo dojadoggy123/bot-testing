d65b6765939bde6d43b70d1fbb6bd3245edcc9c5
MD <microhole10@gmail.com>
2023-06-09 16:58:45 +0800

    adding server

---
+++ README.md
@@ -0,0 +1,7 @@
+## Website to Test for VA chat-bot
+
+This website will be used to test for:
+
+1. email verification
+2. user identification
+3. database
---
+++ main.js
@@ -0,0 +1,26 @@
+let data = {
+    name: "john",
+    id: "55kk99"
+};
+
+console.log(data);
+localStorage.setItem('user',JSON.stringify(data));
+
+
+// retrieve user id from local storage. Go check with other devices
+let user_id = localStorage.getItem("https://web.powerva.microsoft.com/")
+if (user_id) {
+    var parsedData = JSON.parse(data);
+    console.log(parsedData);
+  } else {
+    console.log('No data found');
+  }
+
+  
+document.cookie = `userInfo=${data.name}; expires=Thu, 01 Jan 2024 00:00:00 UTC; path=/`;
+const cookies = document.cookie;
+console.log(cookies)
+
+
+// pva_jopUEClWPD     (same user id for device, go check on other device)
+// pva_jopUEClWPD
