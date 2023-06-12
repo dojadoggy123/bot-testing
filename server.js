d9397ea1bb74640884d3bd2247d329758c4e0750
MD <microhole10@gmail.com>
2023-06-12 09:44:36 +0800

    added some changes to server

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res)=>{
     res.send('bye world')
 })
 
+app.get("/bye", (req,res)=>{
+    res.send('jk im not leaving')
+})
+
+
 app.listen(3000, ()=> {
     console.log("bye world ")
 })
