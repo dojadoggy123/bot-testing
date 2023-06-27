abdb608f8aec224b8f4762cfb77024a8bc3c6be0
MD <microhole10@gmail.com>
2023-06-27 15:59:00 +0800

    -

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ app.use(express.static('public'))
 app.use(express.json())  //accept json requests
 app.use(express.urlencoded({ extended: true }));    //allow complex object & array to be encoded. eg: nested arrays
 
-app.use(
-    // session middleware
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        cookie: {maxAge: 25000},   // 7 days in milliseconds 7*24*60*60*1000
-        saveUninitialized: false
-    })
-)
+// app.use(
+//     // session middleware
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         resave: false,
+//         cookie: {maxAge: 25000},   // 7 days in milliseconds 7*24*60*60*1000
+//         saveUninitialized: false
+//     })
+// )
 
 // set root route to router
 app.use('/', require('./routes/defaultRoute'))
